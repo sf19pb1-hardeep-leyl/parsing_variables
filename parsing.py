2c6c1ffd5c34657cf3ac1604ed72a1c6570c265f
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-10-29 13:15:08 -0400

    Create parsing.py

---
+++ parsing.py
@@ -0,0 +1,17 @@
+"""
+parsing.py
+A function that returns a tuple of items.
+"""
+import sys
+
+def stats(collection):
+    "Return the minimum, maximum, and concatenates the strings in the collection."
+    return (min(collection,key=len), max(collection,key=len), ' '.join(collection)) #outer parens unnecessary
+
+listOfStrings = ["this","is","a","bunch","of","words"]
+shortest, longest, sentence = stats(listOfStrings)
+
+print(f"shortest word = {shortest}")
+print(f"longest = {longest}")
+print(f"whole sentence = {sentence}")
+sys.exit(0)
